ac61db687b66781ee32d6c8b278f2b82f9bb6ebe
elo1lson <81685769+elo1lson@users.noreply.github.com>
2024-10-07 20:35:44 -0300

    feat: improve scren

--- screens/VendasScreen.js
+++ screens/VendasScreen.js
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/MaterialIcons";
 import CupertinoFooter1 from "../components/CupertinoFooter1";
-import Lanches from '../components/Lanches';
+import Lanches from "../components/Lanches";
 
 export default function VendasScreen({ navigation }) {
   return (
     <View style={styles.container}>
       {/* Imagem de destaque */}
-      <Image source={require('../assets/image.png')} style={styles.featuredImage} />
-      
+      <Image source={require("../assets/image.png")} style={styles.featuredImage} />
+
       {/* Container branco */}
       <View style={styles.whiteContainer}>
         {/* Nome da praça */}
@@ -30,12 +30,12 @@ export default function VendasScreen({ navigation }) {
         </View>
         {/* Lanches */}
         <Lanches onPress={(route) => navigation.navigate(route)} />
-        
       </View>
 
       {/* Navegação inferior */}
-      <CupertinoFooter1 style={styles.cupertinoFooter1}
-        onPress={(route) => navigation.navigate(route)} 
+      <CupertinoFooter1
+        style={styles.cupertinoFooter1}
+        onPress={(route) => navigation.navigate(route)}
       ></CupertinoFooter1>
     </View>
   );
@@ -44,53 +44,60 @@ export default function VendasScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#211D1D', // Cor de fundo da tela
+    backgroundColor: "#211D1D", // Cor de fundo da tela
   },
   featuredImage: {
-    width: '100%',
-    height: 200, // Ajuste a altura conforme necessário
-    marginTop: 20,
+    width: "100%",
+    height: 190, // Ajuste a altura conforme necessário
+    marginTop: 0,
+    bottom:0,
+    maxHeight:"25%"
   },
   whiteContainer: {
-    backgroundColor: '#FFFFFF', // Cor de fundo branca
+    backgroundColor: "#FFFFFF", // Cor de fundo branca
     padding: 10, // Adiciona padding para os elementos dentro do container
-    borderRadius: 10, // Adiciona borda arredondada ao container
-    marginBottom: 10, 
-    marginTop: 20,
-    maxHeight: '70%', 
-    width: 402, 
-    height: 519, 
+    borderRadius: 14, // Adiciona borda arredondada ao container
+    borderBottomRightRadius:23,
+    borderBottomLeftRadius:23,
+    marginBottom: 10,
+    width: 402,
+    height: 519,
+    maxHeight: "80%",
     flexShrink: 0,
+    flex:1
   },
   cupertinoFooter1: {
     marginTop: 1,
   },
   categories: {
-    flexDirection: 'row',
-    justifyContent: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
     marginBottom: 10,
     marginTop: 30, // Aumenta a margem acima das categorias
   },
   categoryButton: {
     paddingHorizontal: 15,
     paddingVertical: 5,
-    backgroundColor: '#000000',
-    borderRadius: 20,
-    marginHorizontal: 5,
+    backgroundColor: "#000000",
+    borderRadius: 10,
+    marginHorizontal: 8,
   },
   categoryText: {
     fontSize: 16,
-    color: '#FFF'
+    color: "#FFF",
+    fontWeight:"700"
   },
   statusBar: {
     height: 1, // Ajuste a altura conforme necessário
-    backgroundColor: 'black',
-    marginTop: 10,
+    backgroundColor: "black",
+    marginTop: 10
   },
   title: {
-    fontSize: 16,
-    color: '#000', // Altera a cor do texto para preto
-    textAlign: 'center',
-    marginVertical: 10,
-  },
+    fontSize: 19,
+    color: "#000", // Altera a cor do texto para preto
+    textAlign: "center",
+    marginVertical: 9,
+    fontWeight:'800',
+    letterSpacing:2
+  }
 });
