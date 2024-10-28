de7243cfdf813bc53784699aa5bd0af547345f37
Pedro Henrique Vieira Barreto <112330570+Masterbarreto@users.noreply.github.com>
2024-10-28 10:47:11 -0300

    fet: tela de login parcialmente funcionado

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,15 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, TextInput, TouchableOpacity, KeyboardAvoidingView, ScrollView, Platform, Keyboard } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity, KeyboardAvoidingView, ScrollView, Platform, Keyboard, Alert } from 'react-native';
 import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../firebase';
-import { getDoc, doc } from 'firebase/firestore';
-import { myFS } from '../firebase';
-import LogoPrincipal from '../components/LogoPrincipal.js';
 import styles from '../styles/LoginScreenStyles';
+import LogoPrincipal from '../components/LogoPrincipal'; // Ajuste o caminho conforme necessário
 
+// Schema de validação para o formulário
 const schema = yup.object().shape({
   email: yup.string().email("Email inválido").required("Informe seu email"),
   password: yup.string().required("Informe sua senha").min(6, "A senha deve ter pelo menos 6 dígitos"),
@@ -26,31 +23,50 @@ export default function LoginScreen({ navigation }) {
         throw new Error('Email e senha são obrigatórios.');
       }
 
-      const userCredential = await signInWithEmailAndPassword(auth, data.email, data.password);
-      const user = userCredential.user;
+      // Fazendo requisição para a sua API de login
+      const response = await fetch("http://192.168.0.89:4000/api/v1/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: data.email,
+          password: data.password,
+        }),
+      });
 
-      const userDocRef = doc(myFS, 'users', user.uid);
-      const userDoc = await getDoc(userDocRef);
-      const userData = userDoc.data();
+      if (!response.ok) {
+        // Lidando com erros de resposta da API de forma mais informativa
+        if (response.status === 401) {
+          // Credenciais inválidas
+          Alert.alert('Erro', 'Email ou senha incorretos.');
+        } else {
+          // Outros erros de API
+          Alert.alert('Erro', 'Ocorreu um erro ao fazer login. Tente novamente mais tarde.');
+        }
+        throw new Error('Erro ao fazer login'); 
+      }
 
-      if (userData && userData.role) {
-        if (userData.role === 'Aluno') {
+      const userData = await response.json();
+      console.log( userData); 
+
+      // Agora você tem userData.type após o login
+      if (userData && userData.type) {
+        if (userData.type === 'user') {
           navigation.navigate('Home');
-        } else if (userData.role === 'Lojista') {
+        } else if (userData.type === 'Lojista') {
           navigation.navigate('MinhasLojas');
         } else {
-          console.error('Papel não reconhecido:', userData.role);
+          console.error('Papel não reconhecido:', userData.type);
         }
       } else {
         console.error('Usuário sem papel definido');
       }
 
-      // Limpar os campos de email e senha após o login bem-sucedido
-      reset();
+      reset(); // Limpar os campos após o login (opcional)
     } catch (error) {
-      console.error('Erro ao fazer login:', error);
-      // Limpar os campos de email e senha se houver erro
-      reset();
+      console.error('Erro ao fazer login:', error.message);
+      reset(); // Limpar os campos em caso de erro (opcional)
     }
   };
 
@@ -120,7 +136,7 @@ export default function LoginScreen({ navigation }) {
               )}
             />
 
-            <TouchableOpacity onPress={() => { navigation.navigate('EsqueciaSenha') }}>
+            <TouchableOpacity onPress={() => { navigation.navigate('EsqueciSenha') }}>
               <Text style={styles.forgotPasswordText}>Esqueceu sua senha?</Text>
             </TouchableOpacity>
           </View>
