e42d2505b4b9e7e9de6a0d0c29a847c81c9f7583
elo1lson <81685769+elo1lson@users.noreply.github.com>
2024-11-03 12:45:47 -0300

    feat: rust service

--- web/email_sender/src/main.rs
+++ web/email_sender/src/main.rs
@@ -1,15 +1,41 @@
 use std::net::SocketAddr;
+use sqlx::postgres::PgPoolOptions;
 use tokio;
-mod routes;
 use dotenv::dotenv;
+use std::env;
+use std::sync::Arc;
+mod routes; 
+mod models;
+mod app_state;
+use app_state::AppState;
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
-    let app = routes::create_router();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL not set");
+
+    let db_pool = match PgPoolOptions::new()
+    .max_connections(5)
+    .connect(&database_url).await {
+        Ok(db_pool) => {
+            println!("✅ Conexão com o banco de dados bem-sucedida!");
+            db_pool
+        }
+        Err(err) => {
+            println!("❌ Falha ao conectar ao banco de dados: {:?}", err);
+            std::process::exit(1);
+        }
+
+    };
+
+    let app_state = Arc::new(AppState { db_pool: db_pool.into() });
+
+    let app = routes::create_router(app_state.clone());
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3001));
-    println!("servidor rodando em http://{}", addr);
+    println!("Servidor rodando em http://{}", addr);
+    
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
