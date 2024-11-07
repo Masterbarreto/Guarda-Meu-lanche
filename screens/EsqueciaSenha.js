256e1b2b529fde521bc406c5d2eb4cf65430b734
Pedro Henrique Vieira Barreto <112330570+Masterbarreto@users.noreply.github.com>
2024-11-07 10:12:49 -0300

    Fx:coreção de emergencia

--- screens/EsqueciaSenha.js
+++ screens/EsqueciaSenha.js
@@ -8,42 +8,73 @@ import {
   Alert,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { sendPasswordResetEmail } from "firebase/auth";
+import { auth } from "../firebase";
 import GoBack from "../components/Back";
 import styles from "../styles/EsqueciaSenhaStyles"; // Importa os estilos
 import EmailCode from "./EmailCode";
+import * as Yup from "yup";
+import axios from "axios";
+import { API_URL } from "@env";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().email("Email inválido").required("O email é obrigatório"),
+});
 
 export default function EsqueciaSenha() {
-  const navigation = useNavigation();
   const [email, setEmail] = useState("");
+  const [validationError, setValidationError] = useState("");
+
+  const navigation = useNavigation();
   const [showResetMessage, setShowResetMessage] = useState(false);
 
   const handlePasswordReset = async () => {
-    if (email !== "") {
-      try {
-        setShowResetMessage(true); 
-        Alert.alert("E-mail de redefinição de senha enviado com sucesso");
-      } catch (error) {
-        console.error(error);
-        Alert.alert("Erro ao enviar e-mail de redefinição de senha");
+    try {
+      await validationSchema.validate({ email });
+
+      const userData = await sendMail(email);
+      console.log(userData);
+      
+      if (userData.error?.status) {
+        if (userData.error.status == 404) {
+          setValidationError("Email não encontrado.");
+          return
+        }
+        
       }
-    } else {
-      Alert.alert("Por favor, informe um e-mail válido.");
+
+      setShowResetMessage(true);
+    } catch (error) {
+      console.log(error);
+      
+      if (error.name === "ValidationError") {
+        setValidationError(error.message);
+      } else {
+        console.error("Erro inesperado:", error);
+        setValidationError("Ocorreu um erro inesperado. Tente novamente.");
+      }
     }
   };
 
+  const sendMail = async (email) => {
+    const url = `${API_URL}/reset_password?email=${email}`;
+
+    return axios
+      .get(url)
+      .then((data) => data.data)
+      .catch((e) => e.response.data);
+  };
   return (
     <KeyboardAvoidingView style={styles.container} behavior="padding">
       <GoBack />
       <View style={styles.contentContainer}>
         {showResetMessage ? (
-          // Exibe a mensagem de confirmação após o reset de senha
           <EmailCode userEmail={email} />
         ) : (
-          // Formulário padrão de redefinição de senha
           <>
             <Text style={styles.title}>Esqueceu a senha?</Text>
             <Text style={styles.texto}>
-              Digite seu e-mail para o processo de verificação. Dentro de alguns minutos,
+              Digite seu e-mail para o processo de verificação, dentro de alguns minutos
               enviaremos o código para o seu e-mail.
             </Text>
             <Text style={styles.email}>Email</Text>
@@ -53,6 +84,13 @@ export default function EsqueciaSenha() {
               onChangeText={setEmail}
               style={styles.input}
             />
+            {validationError ? (
+              <Text
+                style={{ color: "red", marginTop: 3, fontSize: 12, fontWeight: "700" }}
+              >
+                {validationError}
+              </Text>
+            ) : null}
             <TouchableOpacity onPress={handlePasswordReset} style={styles.button}>
               <Text style={styles.buttonText}>Continue</Text>
             </TouchableOpacity>
--- web/api/server/server.js
+++ web/api/server/server.js
@@ -1,17 +1,14 @@
 import "dotenv/config";
 import "../shared/yup.js";
 import express from "express";
-import cors from 'cors';
 import { apiRouter } from "../routes/api.js";
 import { router } from "../routes/router.js";
 import { apiRedirect } from "./apiRedirect.js";
 import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import path from "path";
-
+import { StatusCodes } from "http-status-codes";
 const server = express();
-
-server.use(cors());
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
@@ -19,33 +16,37 @@ server.use(apiRedirect);
 server.use("/api/v1", apiRouter);
 server.use(router);
 
-server.use("/pages",express.static(path.resolve("./public/pages")));
+server.use("/pages", express.static(path.resolve("./public/pages")));
 
-
-server.use((req, res, next)=>{
-  return res.sendFile(path.resolve("public","pages","404.html"))
-
-})
+server.use((req, res, next) => {
+    //   return res.sendFile(path.resolve("public","pages","404.html"))
+    return res.status(StatusCodes.NOT_FOUND).json({
+        error: {
+            message: "rota não encontrada",
+            status: StatusCodes.NOT_FOUND,
+        },
+    });
+});
 const options = {
-  definition: { 
-    openapi: "3.1.0",
-    info: {
-      title: "Guarda meu lanche Api",
-      version: "0.1.0",
-      description: "Uma simples documentação da nossa api.",
+    definition: {
+        openapi: "3.1.0",
+        info: {
+            title: "Guarda meu lanche Api",
+            version: "0.1.0",
+            description: "Uma simples documentação da nossa api.",
 
-      contact: {
-        name: "elo1lson",
-        url: "https://github.com/elo1lson",
-      },
+            contact: {
+                name: "elo1lson",
+                url: "https://github.com/elo1lson",
+            },
+        },
+        servers: [
+            {
+                url: "http://localhost:3000",
+            },
+        ],
     },
-    servers: [
-      {
-        url: "http://localhost:3000",
-      },
-    ],
-  },
-  apis: ["./controllers/*/*.js", "./routes/*.js"],
+    apis: ["./controllers/*/*.js", "./routes/*.js"],
 };
 
 const specs = swaggerJsdoc(options);
